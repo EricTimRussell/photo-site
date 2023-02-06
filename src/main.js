f97dcc64963eb167c75fdf56c7e61433d92f8763
Eric Russell <Erictim.Russell@live.com>
2023-02-06 15:55:10 -0700

    boiler cleanup and tailwindcss

--- src/main.js
+++ src/main.js
@@ -4,8 +4,7 @@ import { VueFire } from 'vuefire'
 import { firebaseApp } from '@/firebase.js'
 import App from './App.vue'
 import router from './router'
-
-import './assets/main.css'
+import './assets/style.css'
 
 const app = createApp(App)
 
