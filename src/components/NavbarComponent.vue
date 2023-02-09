3614dc744f342f97448da185502aa17262a8e22d
Eric Russell <Erictim.Russell@live.com>
2023-02-09 13:19:40 -0700

    router views

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -2,17 +2,17 @@
   <div class="bg-indigo-50 shadow-xl">
     <nav class="grid grid-cols-6 content-center h-20">
       <div class="col-start-2">
-        <RouterLink to="/">
+        <router-link to="/">
           <h1 class="text-3xl text-black">Website Name(HOME)</h1>
-        </RouterLink>
+        </router-link>
       </div>
       <div class="flex col-start-4 gap-10">
-        <RouterLink to="/collections">
+        <router-link to="/collections">
           <h2 class="text-2xl">Collections</h2>
-        </RouterLink>
-        <RouterLink to="/market">
+        </router-link>
+        <router-link to="/market">
           <h2 class="text-2xl">Market</h2>
-        </RouterLink>
+        </router-link>
       </div>
       <div class="col-start-6 text-end pr-10">
         <h2 class="text-xl">Login/Create</h2>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
+import HomeView from '../views/HomeView.vue'
+import CollectionsView from '../views/CollectionsView.vue'
+import MarketView from '../views/MarketView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,15 +9,18 @@ const router = createRouter({
     {
       path: '/',
       name: 'Home',
+      component: HomeView
     },
     {
       path: '/collections',
       name: 'Collections',
+      component: CollectionsView
     },
     {
       path: '/market',
       name: 'Market',
-    },
+      component: MarketView
+    }
   ]
 })
 
